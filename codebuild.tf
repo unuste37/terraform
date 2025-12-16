5492be47278250f8a0e41414abdc2f91ad1d5558
unuste37 <91313323+unuste37@users.noreply.github.com>
2025-12-16 08:53:55 +0530

    Add S3 bucket and IAM role for CodeBuild
    Added AWS S3 bucket and IAM role for CodeBuild integration. Updated IAM role policy to include CodePipeline service.

--- codebuild.tf
+++ codebuild.tf
@@ -1,3 +1,42 @@
+resource "aws_s3_bucket" "codepipeline_bucket" {
+  bucket        = "${var.project_name}-artifacts-${random_id.suffix.hex}"
+  force_destroy = true
+  tags          = local.tags
+}
+
+resource "random_id" "suffix" {
+  byte_length = 3
+}
+
+resource "aws_iam_role" "codebuild_role" {
+  name = "${var.project_name}-cb-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect = "Allow"
+      Action = "sts:AssumeRole"
+      Principal = {
+        # *** CORRECTION HERE ***
+        # The Principal should be an array (list) of services allowed to assume the role.
+        # We need to add "codepipeline.amazonaws.com" to resolve the error.
+        Service = [
+          "codepipeline.amazonaws.com",
+          "codebuild.amazonaws.com"
+        ]
+      }
+    }]
+  })
+}
+
+# NOTE: Using "AdministratorAccess" is a broad permission. 
+# While convenient for labs, use specific policies (like AWSCodePipelineServiceRole, 
+# AWSCodeBuildDeveloperAccess, etc.) in production environments.
+resource "aws_iam_role_policy_attachment" "codebuild_attach" {
+  role       = aws_iam_role.codebuild_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess"
+}
+
 resource "aws_codebuild_project" "terraform_build" {
   name         = "${var.project_name}-build"
   service_role = aws_iam_role.codebuild_role.arn
@@ -10,10 +49,10 @@ resource "aws_codebuild_project" "terraform_build" {
     type = "NO_ARTIFACTS"
   }
   environment {
-    compute_type                = "BUILD_GENERAL1_SMALL"
-    image                       = "aws/codebuild/standard:7.0"
-    type                        = "LINUX_CONTAINER"
-    privileged_mode             = true
+    compute_type    = "BUILD_GENERAL1_SMALL"
+    image           = "aws/codebuild/standard:7.0"
+    type            = "LINUX_CONTAINER"
+    privileged_mode = true
   }
 }
 
@@ -37,11 +76,11 @@ resource "aws_codepipeline" "terraform_pipeline" {
       version          = "1"
       output_artifacts = ["source_output"]
       configuration = {
-        Owner      = "unuste37"
-        Repo       = "terraform"
-        Branch     = "main"
+        Owner  = "unuste37"
+        Repo   = "terraform"
+        Branch = "main"
         #OAuthToken = var.github_token
-        OAuthToken = "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"
+        OAuthToken = "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"
       }
     }
   }
@@ -49,12 +88,12 @@ resource "aws_codepipeline" "terraform_pipeline" {
   stage {
     name = "Build"
     action {
-      name             = "Terraform"
-      category         = "Build"
-      owner            = "AWS"
-      provider         = "CodeBuild"
-      input_artifacts  = ["source_output"]
-      version          = "1"
+      name            = "Terraform"
+      category        = "Build"
+      owner           = "AWS"
+      provider        = "CodeBuild"
+      input_artifacts = ["source_output"]
+      version         = "1"
       configuration = {
         ProjectName = aws_codebuild_project.terraform_build.name
       }
